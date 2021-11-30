d7571f4673f908b00e9ffa4316f411b22b7c8782
Lyz03 <lizoe@netcourrier.com>
2021-11-30 11:50:06 +0100

    exo 86

--- app.js
+++ app.js
@@ -31,10 +31,17 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [peugeot, citroen, ford], // Vos voitures dans cette propriété !
 }
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
+
+voitures.collection.forEach(function (value) {
+    value.ouvrirPorte();
+     const colorSpan = document.createElement('span');
+     colorSpan.innerText = value.color + '\n';
+     document.querySelector('p').appendChild(colorSpan);
+});
 
 
